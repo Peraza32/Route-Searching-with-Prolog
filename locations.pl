cebf6f805ae786fbe95e96870b097e7165ee07a3
Victor <00143320@uca.edu.sv>
2023-09-25 10:45:39 -0600

    Add Instituciones educativas to Lugares in BC :construction:

---
+++ locations.pl
@@ -0,0 +1,9 @@
+%% Lugares
+%% Instituciones educativas
+lugar(uca).
+lugar(escuela_alemana).
+lugar(instituto_hermanas_somascas).
+lugar(instituto_tecnico_emiliani).
+lugar(colegio_augusto_walte).
+
+
