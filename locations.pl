13abf632441399d4eba18bb59b1838273737dda4
Victor <00143320@uca.edu.sv>
2023-09-25 11:00:29 -0600

    Add Centros de Salud to Lugares in BC :construction:

--- locations.pl
+++ locations.pl
@@ -6,4 +6,12 @@ lugar(instituto_hermanas_somascas).
 lugar(instituto_tecnico_emiliani).
 lugar(colegio_augusto_walte).
 
+%% Centros de Salud
+lugar(instituto_salvadoreño_del_seguro_social). 
+lugar(farmacia_economica_la_sultana).
+lugar(analiza_laboratio_clinico).
+lugar(bioinnova).
+lugar(hospital_el_salvador).
+
+
 
