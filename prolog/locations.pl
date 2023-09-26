8fb6f9d1e1794a74aa98953f2d6ede1f63c18e81
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-09-25 23:43:15 -0600

    Finish connection with the knowledge base between Java and Prolog using the eclipse ide

--- prolog/locations.pl
+++ prolog/locations.pl
@@ -1,33 +1,33 @@
-%% Lugares
-%% Instituciones educativas
+% Lugares
+% Instituciones educativas
 lugar(uca).
 lugar(escuela_alemana).
 lugar(instituto_hermanas_somascas).
 lugar(instituto_tecnico_emiliani).
 lugar(colegio_augusto_walte).
 
-%% Centros de Salud
-lugar(instituto_salvadoreño_del_seguro_social). 
+% Centros de Salud
+lugar(instituto_salvadoreno_del_seguro_social). 
 lugar(farmacia_economica_la_sultana).
 lugar(analiza_laboratio_clinico).
 lugar(bioinnova).
 lugar(hospital_el_salvador).
 
-%% Gasolineras
+% Gasolineras
 lugar(uno_la_sultana).
 lugar(uno_las_lomas).
 lugar(uno_los_proceres).
 lugar(uno_monumental).
 lugar(puma_los_proceres).
 
-%% Parques, eco centros o centros deportivos 
+% Parques, eco centros o centros deportivos 
 lugar(cancah_emiliani).
 lugar(parque_lomas_de_san_francisco).
 lugar(parque_bicentenario).
 lugar(parque_central_antiguo_cuscatlan).
 lugar(estadio_cuscatlan).
 
-%% Centros comerciales
+% Centros comerciales
 lugar(multiplaza).
 lugar(centro_comercial_las_cascadas).
 lugar(la_gran_via).
