54fee2f7a04c5229e299200200a07955c3b746b6
Victor <00143320@uca.edu.sv>
2023-10-08 10:30:27 -0600

    change location of locations.pl :truck:

--- prolog/locations.pl
+++ prolog/locations.pl
@@ -8,20 +8,20 @@ lugar(colegio_augusto_walte).
 
 % Centros de Salud
 lugar(instituto_salvadoreno_del_seguro_social). 
-lugar(farmacia_economica_la_sultana).
+lugar(centro_dental_la_sultana).
 lugar(analiza_laboratio_clinico).
 lugar(bioinnova).
-lugar(hospital_el_salvador).
+lugar(cruz_verde_antiguo_cuscatlan).
 
 % Gasolineras
 lugar(uno_la_sultana).
 lugar(uno_las_lomas).
 lugar(uno_los_proceres).
-lugar(uno_monumental).
+lugar(uno_autopista_sur).
 lugar(puma_los_proceres).
 
-% Parques, eco centros o centros deportivos 
-lugar(cancah_emiliani).
+%% Parques, eco centros o centros deportivos 
+lugar(cancha_emiliani).
 lugar(parque_lomas_de_san_francisco).
 lugar(parque_bicentenario).
 lugar(parque_central_antiguo_cuscatlan).
@@ -35,3 +35,334 @@ lugar(plaza_tunal).
 lugar(centro_comercial_jardines).
 
 
+%% Calles y segmentos de calle 
+
+%% Calle cantabrico, 6 partes
+calles(calle_cantabrico_s1).
+calles(calle_cantabrico_s2).
+calles(calle_cantabrico_s3).
+calles(calle_cantabrico_s4).
+calles(calle_cantabrico_s5).
+calles(calle_cantabrico_s6).
+
+%% Calle Mediterraneo, 16 secciones
+calles(calle_mediterraneo_s1).
+calles(calle_mediterraneo_s2).
+calles(calle_mediterraneo_s3).
+calles(calle_mediterraneo_s4).
+calles(calle_mediterraneo_s5).
+calles(calle_mediterraneo_s6).
+calles(calle_mediterraneo_s7).
+calles(calle_mediterraneo_s8).
+calles(calle_mediterraneo_s9).
+calles(calle_mediterraneo_s10).
+calles(calle_mediterraneo_s11).
+calles(calle_mediterraneo_s12).
+calles(calle_mediterraneo_s13).
+calles(calle_mediterraneo_s14).
+calles(calle_mediterraneo_s15).
+calles(calle_mediterraneo_s16).
+
+%% Avenida Albert Einstein, 5 seccion
+calles(avenida_albert_einstein_s1).
+calles(avenida_albert_einstein_s2).
+calles(avenida_albert_einstein_s3).
+calles(avenida_albert_einstein_s4).
+calles(avenida_albert_einstein_s5).
+
+%% Boulevard La Sultana, 5 secciones
+calles(boulevard_la_sultana_s1).
+calles(boulevard_la_sultana_s2).
+calles(boulevard_la_sultana_s3).
+calles(boulevard_la_sultana_s4).
+calles(boulevard_la_sultana_s5).
+
+%% Avenida Las Palmeras, 3 secciones
+calles(avenida_las_palmeras_s1).
+calles(avenida_las_palmeras_s2).
+calles(avenida_las_palmeras_s3).
+
+%% 1ra Avenida Norte, 1 seccion
+calles(primera_avenida_norte_s1).
+
+%% 3 Avenida Norte, 5 secciones
+calles(tercera_avenida_norte_s1).
+calles(tercera_avenida_norte_s2).
+calles(tercera_avenida_norte_s3).
+calles(tercera_avenida_norte_s4).
+calles(tercera_avenida_norte_s5).
+
+%% 1 Calle Oriente, 5 secciones
+calles(primera_calle_oriente_s1).
+calles(primera_calle_oriente_s2).
+calles(primera_calle_oriente_s3).
+calles(primera_calle_oriente_s4).
+calles(primera_calle_oriente_s5).
+
+%% 1 Calle Poniente, 1 seccion
+calles(primera_calle_poniente_s1).
+
+%% Boulevard Walter Thilo Deininger, 7 secciones
+calles(boulevard_walter_thilo_deininger_s1).
+calles(boulevard_walter_thilo_deininger_s2).
+calles(boulevard_walter_thilo_deininger_s3).
+calles(boulevard_walter_thilo_deininger_s4).
+calles(boulevard_walter_thilo_deininger_s5).
+calles(boulevard_walter_thilo_deininger_s6).
+calles(boulevard_walter_thilo_deininger_s7).
+
+%% Calle Cuscatlan Poniente, 1 seccion
+calles(calle_cuscatlan_poniente_s1).
+
+%% Carretera Panamericana, 14 secciones
+calles(carretera_panamericana_s1).
+calles(carretera_panamericana_s2).
+calles(carretera_panamericana_s3).
+calles(carretera_panamericana_s4).
+calles(carretera_panamericana_s5).
+calles(carretera_panamericana_s6).
+calles(carretera_panamericana_s7).
+calles(carretera_panamericana_s8).
+calles(carretera_panamericana_s9).
+calles(carretera_panamericana_s10).
+calles(carretera_panamericana_s11).
+calles(carretera_panamericana_s12).
+calles(carretera_panamericana_s13).
+calles(carretera_panamericana_s14).
+
+%% Boulevard los Proceres, 5 secciones
+calles(boulevard_los_proceres_s1).
+calles(boulevard_los_proceres_s2).
+calles(boulevard_los_proceres_s3).
+calles(boulevard_los_proceres_s4).
+calles(boulevard_los_proceres_s5).
+
+%% Boulevard Monsenor Romero, 4 secciones
+calles(boulevard_monsenor_romero_s1).
+calles(boulevard_monsenor_romero_s2).
+calles(boulevard_monsenor_romero_s3).
+calles(boulevard_monsenor_romero_s4).
+
+
+%% Boulevard Altamira, 1 seccion
+calles(boulevard_altamira_s1).
+
+%% Calle Los Viveros, 1 seccion
+calles(calle_los_viveros_s1).
+
+%% Calle Los Estadi, 1 seccion
+calles(calle_los_estadi_s1).
+
+%% Calle Antigua Ferrocaril, 2 secciones
+calles(calle_antigua_ferrocaril_s1).
+calles(calle_antigua_ferrocaril_s2).
+
+%% Calle San Jeronimo Emiliani, 1 seccion
+calles(calle_san_jeronimo_emiliani_s1).
+
+%% Calle las Azucenas, 1 seccion
+calles(calle_las_azucenas_s1).
+
+%% Avenida Rio Amazonas, 1 seccion
+calles(avenida_rio_amazonas_s1).
+
+%% TODO: asigar las coordenadas
+%% Coordenadas de los lugares, x, y
+%% Instituciones educativas
+%% centros de salud
+%% gasolineras
+%% parques, eco centros o centros deportivos
+%% centros comerciales
+%% calles y segmentos de calle
+
+
+%% Conexiones entre los lugares y calles
+%% Instituciones educativas
+%% UCA 
+conecta(uca, calle_cantabrico_s4).
+conecta(uca, boulevard_la_sultana_s3).
+conecta(uca, boulevard_los_proceres_s4).
+conecta(calle_cantabrico_s4, uca).
+conecta(boulevard_la_sultana_s3, uca).
+conecta(boulevard_los_proceres_s4, uca).
+
+%% Escuela Alemana
+conecta(escuela_alemana,calle_mediterraneo_s9).
+conecta(calle_mediterraneo_s9, escuela_alemana).
+
+%% Instituto Hermanas Somascas
+conecta(instituto_hermanas_somascas, calle_mediterraneo_s13).
+conecta(calle_mediterraneo_s13, instituto_hermanas_somascas).
+
+%% Instituto Tecnico Emiliani
+conecta(instituto_tecnico_emiliani, carretera_panamericana_s3).
+conecta(carretera_panamericana_s3, instituto_tecnico_emiliani).
+
+%% Colegio Augusto Walte
+conecta(colegio_augusto_walte, calle_san_jeronimo_emiliani_s1).
+conecta(calle_san_jeronimo_emiliani_s1, colegio_augusto_walte).
+
+%% Centros de Salud
+%% Instituto Salvadoreno del Seguro Social
+conecta(instituto_salvadoreno_del_seguro_social, boulevard_monsenor_romero_s3).
+conecta(boulevard_monsenor_romero_s3, instituto_salvadoreno_del_seguro_social).
+
+%% Centro Dental La Sultana
+conecta(centro_dental_la_sultana, calle_las_azucenas_s1).
+conecta(calle_las_azucenas_s1, centro_dental_la_sultana).
+
+%% Analiza Laboratorio Clinico
+conecta(analiza_laboratorio_clinico, carretera_panamericana_s5).
+conecta(carretera_panamericana_s5, analiza_laboratorio_clinico).
+
+%% Bioinnova
+conecta(bioinnova, avenida_las_palmeras_s1). 
+conecta(avenida_las_palmeras_s1, bioinnova).
+
+%% Cruz Verde Antiguo Cuscatlan
+conecta(cruz_verde_antiguo_cuscatlan, tercera_avenida_norte_s3).
+conecta(tercera_avenida_norte_s3, cruz_verde_antiguo_cuscatlan).
+
+%% Gasolineras
+%% Uno La Sultana
+conecta(uno_la_sultana, calle_mediterraneo_s11).
+conecta(calle_mediterraneo_s11, uno_la_sultana).
+
+%% Uno Las Lomas
+conecta(uno_las_lomas, avenida_albert_einstein_s2).
+conecta(avenida_albert_einstein_s2, uno_las_lomas).
+
+%% Uno Los Proceres
+conecta(uno_los_proceres, boulevard_los_proceres_s2).
+conecta(boulevard_los_proceres_s2, uno_los_proceres).
+
+%% Uno Autopista Sur
+conecta(uno_autopista_sur, boulevard_los_proceres_s1).
+conecta(boulevard_los_proceres_s1, uno_autopista_sur).
+
+%% Puma Los Proceres
+conecta(puma_los_proceres, boulevard_los_proceres_s5).
+conecta(boulevard_los_proceres_s5, puma_los_proceres).
+
+%% Parques, eco centros o centros deportivos
+%% Cancha Emiliani
+conecta(cancha_emiliani, calle_mediterraneo_s13).
+conecta(calle_mediterraneo_s13, cancha_emiliani).
+
+%% Parque Lomas de San Francisco
+conecta(parque_lomas_de_san_francisco, calle_mediterraneo_s1).
+conecta(calle_mediterraneo_s1, parque_lomas_de_san_francisco).
+
+%% Parque Bicentenario
+conecta(parque_bicentenario,boulevard_monsenor_romero_s4).
+conecta(boulevard_monsenor_romero_s4, parque_bicentenario).
+
+%% Parque Central Antiguo Cuscatlan
+conecta(parque_central_antiguo_cuscatlan, tercera_avenida_norte_s5).
+conecta(tercera_avenida_norte_s5, parque_central_antiguo_cuscatlan).
+conecta(parque_central_antiguo_cuscatlan, boulevard_walter_thilo_deininger_s1).
+conecta(boulevard_walter_thilo_deininger_s1, parque_central_antiguo_cuscatlan).
+
+%% Estadio Cuscatlan    
+conecta(estadio_cuscatlan,calle_los_estadi_s1).
+conecta(calle_los_estadi_s1, estadio_cuscatlan).
+
+%% Centros comerciales
+%% Multiplaza
+conecta(multiplaza, carretera_panamericana_s6).
+conecta(carretera_panamericana_s6, multiplaza).
+
+%% Centro Comercial Las Cascadas
+conecta(centro_comercial_las_cascadas, carretera_panamericana_s10).
+conecta(carretera_panamericana_s10, centro_comercial_las_cascadas).
+
+%% La Gran Via
+conecta(la_gran_via,carretera_panamericana_s11).
+conecta(carretera_panamericana_s11, la_gran_via).
+
+%% Plaza Tunal
+conecta(plaza_tunal, avenida_albert_einstein_s2).
+conecta(avenida_albert_einstein_s2, plaza_tunal).
+
+%% Centro Comercial Jardines
+conecta(centro_comercial_jardines, calle_mediterraneo_s7).
+conecta(calle_mediterraneo_s7, centro_comercial_jardines).
+
+%% Calles y segmentos de calle
+%% Calle Cantabrico
+conecta(calle_cantabrico_s1, calle_cantabrico_s2).
+conecta(calle_cantabrico_s2, calle_cantabrico_s3).
+conecta(calle_cantabrico_s2, calle_cantabrico_s6).
+conecta(calle_cantabrico_s3, calle_cantabrico_s4).
+conecta(calle_cantabrico_s4, calle_cantabrico_s5).
+
+conecta(calle_cantabrico_s2, calle_cantabrico_s1).
+conecta(calle_cantabrico_s3, calle_cantabrico_s2).
+conecta(calle_cantabrico_s6, calle_cantabrico_s2).
+conecta(calle_cantabrico_s4, calle_cantabrico_s3).
+conecta(calle_cantabrico_s5, calle_cantabrico_s4).
+
+% uniones 
+conecta(calle_cantabrico_s1, calle_mediterraneo_s5).
+conecta(calle_mediterraneo_s5, calle_cantabrico_s1).
+
+conecta(calle_cantabrico_s6, calle_mediterraneo_s4).
+conecta(calle_mediterraneo_s4, calle_cantabrico_s6).
+
+conecta(calle_cantabrico_s1,avenida_albert_einstein_s2).
+conecta(avenida_albert_einstein_s2, calle_cantabrico_s1).
+
+%% Calle Mediterraneo
+conecta(calle_mediterraneo_s1, calle_mediterraneo_s2).
+conecta(calle_mediterraneo_s2, calle_mediterraneo_s3).
+conecta(calle_mediterraneo_s3, calle_mediterraneo_s4).
+conecta(calle_mediterraneo_s4, calle_mediterraneo_s5).
+conecta(calle_mediterraneo_s5, calle_mediterraneo_s6).
+conecta(calle_mediterraneo_s6, calle_mediterraneo_s7).
+conecta(calle_mediterraneo_s7, calle_mediterraneo_s8).
+conecta(calle_mediterraneo_s8, calle_mediterraneo_s9).
+conecta(calle_mediterraneo_s9, calle_mediterraneo_s10).
+conecta(calle_mediterraneo_s10, calle_mediterraneo_s11).
+conecta(calle_mediterraneo_s11, calle_mediterraneo_s12).
+conecta(calle_mediterraneo_s12, calle_mediterraneo_s13).
+conecta(calle_mediterraneo_s13, calle_mediterraneo_s14).
+conecta(calle_mediterraneo_s14, calle_mediterraneo_s15).
+conecta(calle_mediterraneo_s14, calle_mediterraneo_s16).
+
+conecta(calle_mediterraneo_s2, calle_mediterraneo_s1).
+conecta(calle_mediterraneo_s3, calle_mediterraneo_s2).
+conecta(calle_mediterraneo_s4, calle_mediterraneo_s3).
+conecta(calle_mediterraneo_s5, calle_mediterraneo_s4).
+conecta(calle_mediterraneo_s6, calle_mediterraneo_s5).
+conecta(calle_mediterraneo_s7, calle_mediterraneo_s6).
+conecta(calle_mediterraneo_s8, calle_mediterraneo_s7).
+conecta(calle_mediterraneo_s9, calle_mediterraneo_s8).
+conecta(calle_mediterraneo_s10, calle_mediterraneo_s9).
+conecta(calle_mediterraneo_s11, calle_mediterraneo_s10).
+conecta(calle_mediterraneo_s12, calle_mediterraneo_s11).
+conecta(calle_mediterraneo_s13, calle_mediterraneo_s12).
+conecta(calle_mediterraneo_s14, calle_mediterraneo_s13).
+conecta(calle_mediterraneo_s15, calle_mediterraneo_s14).
+conecta(calle_mediterraneo_s16, calle_mediterraneo_s14).
+
+% uniones
+conecta(calle_mediterraneo_s2,avenida_albert_einstein_s2).
+conecta(avenida_albert_einstein_s2, calle_mediterraneo_s2).
+
+conecta(calle_mediterraneo_s2,avenida_albert_einstein_s3).
+conecta(avenida_albert_einstein_s3, calle_mediterraneo_s2).
+
+conecta(calle_mediterraneo_s6,avenida_rio_amazonas_s1).
+conecta(avenida_rio_amazonas_s1, calle_mediterraneo_s6).
+
+conecta(calle_mediterraneo_s12,avenida_las_palmeras_s1).
+conecta(avenida_las_palmeras_s1, calle_mediterraneo_s12).
+
+conecta(calle_mediterraneo_s15,carretera_panamericana_s3).
+conecta(carretera_panamericana_s3,calle_mediterraneo_s16).
+
+conecta(calle_mediterraneo_s12.boulevard_la_sultana_s1).
+conecta(boulevard_la_sultana_s1, calle_mediterraneo_s12).
+
+%% Avenida Albert Einstein
+conecta(avenida_albert_einstein_s1, avenida_albert_einstein_s2).
