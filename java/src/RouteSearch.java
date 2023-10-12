bea8f44ae21fdbee9cb9ec150e17483335b02fdf
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-10-12 14:47:44 -0600

    Add a way to check if the enter route is valid or not

--- java/src/RouteSearch.java
+++ java/src/RouteSearch.java
@@ -11,18 +11,21 @@ public class RouteSearch {
 				new Term[] { new Atom(
 						"C:\\Users\\gilbe\\Desktop\\UCA\\ciclo-02-2023\\programacion-declarativa\\primer-proyecto\\Route-Searching-with-Prolog\\prolog\\locations.pl") });
 
-		System.out.println("consult " + (q1.hasSolution() ? "succeeded" : "failed"));
-		System.out.println("");
-
 		Variable Route = new Variable("Route");
-		Query q2 = new Query("ir_hacia", new Term[] { new Atom("sala"), new Atom("quiosco"), Route });
+		Query q2 = new Query("ir_hacia", new Term[] { new Atom("cocina"), new Atom("qwqw"), Route });
 
 		Map<String, Term>[] solution;
 
-		solution = q2.allSolutions();
+		if (q1.hasSolution() && q2.hasSolution()) {
+			solution = q2.allSolutions();
 
-		for (int i = 0; i < solution.length; i++) {
-			System.out.println("Route = " + solution[i].get("Route"));
+			for (int i = 0; i < solution.length; i++) {
+				System.out.println("Route = " + solution[i].get("Route"));
+			}
+		} else {
+			System.out.println("consult q1 or q2 has an issue, please checl");
+			System.out.println("");
 		}
+
 	}
 }
