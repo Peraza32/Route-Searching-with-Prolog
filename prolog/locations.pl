46d5bca5f967bbad28c22909ef41ff24601449ae
Gil Campos <47683202+Gil-Campos@users.noreply.github.com>
2023-10-13 16:16:31 -0600

    Added the code to return the coordinates in a form of list

--- prolog/locations.pl
+++ prolog/locations.pl
@@ -38,137 +38,137 @@ lugar(centro_comercial_jardines).
 %% Calles y segmentos de calle
 
 %% Calle cantabrico, 6 partes
-calles(calle_cantabrico_s1).
-calles(calle_cantabrico_s2).
-calles(calle_cantabrico_s3).
-calles(calle_cantabrico_s4).
-calles(calle_cantabrico_s5).
-calles(calle_cantabrico_s6).
+calles(calle_cantabrico_s1, 01, 01).
+calles(calle_cantabrico_s2, 02, 02).
+calles(calle_cantabrico_s3, 03, 03).
+calles(calle_cantabrico_s4, 04, 04).
+calles(calle_cantabrico_s5, 05, 05).
+calles(calle_cantabrico_s6, 06, 06).
 
 %% Calle Mediterraneo, 16 secciones
-calles(calle_mediterraneo_s1).
-calles(calle_mediterraneo_s2).
-calles(calle_mediterraneo_s3).
-calles(calle_mediterraneo_s4).
-calles(calle_mediterraneo_s5).
-calles(calle_mediterraneo_s6).
-calles(calle_mediterraneo_s7).
-calles(calle_mediterraneo_s8).
-calles(calle_mediterraneo_s9).
-calles(calle_mediterraneo_s10).
-calles(calle_mediterraneo_s11).
-calles(calle_mediterraneo_s12).
-calles(calle_mediterraneo_s13).
-calles(calle_mediterraneo_s14).
-calles(calle_mediterraneo_s15).
-calles(calle_mediterraneo_s16).
+calles(calle_mediterraneo_s1, 07, 07).
+calles(calle_mediterraneo_s2, 08, 08).
+calles(calle_mediterraneo_s3, 09, 09).
+calles(calle_mediterraneo_s4, 10, 10).
+calles(calle_mediterraneo_s5, 11, 11).
+calles(calle_mediterraneo_s6, 12, 12).
+calles(calle_mediterraneo_s7, 13, 13).
+calles(calle_mediterraneo_s8, 14, 14).
+calles(calle_mediterraneo_s9, 15, 15).
+calles(calle_mediterraneo_s10, 16, 16).
+calles(calle_mediterraneo_s11, 17, 17).
+calles(calle_mediterraneo_s12, 18, 18).
+calles(calle_mediterraneo_s13, 19, 19).
+calles(calle_mediterraneo_s14, 20, 20).
+calles(calle_mediterraneo_s15, 21, 21).
+calles(calle_mediterraneo_s16, 22, 22).
 
 %% Avenida Albert Einstein, 5 seccion
-calles(avenida_albert_einstein_s1).
-calles(avenida_albert_einstein_s2).
-calles(avenida_albert_einstein_s3).
-calles(avenida_albert_einstein_s4).
-calles(avenida_albert_einstein_s5).
+calles(avenida_albert_einstein_s1, 23, 23).
+calles(avenida_albert_einstein_s2, 23, 23).
+calles(avenida_albert_einstein_s3, 23, 23).
+calles(avenida_albert_einstein_s4, 24, 24).
+calles(avenida_albert_einstein_s5, 25, 25).
 
 %% Boulevard La Sultana, 5 secciones
-calles(boulevard_la_sultana_s1).
-calles(boulevard_la_sultana_s2).
-calles(boulevard_la_sultana_s3).
-calles(boulevard_la_sultana_s4).
-calles(boulevard_la_sultana_s5).
+calles(boulevard_la_sultana_s1, 26, 26).
+calles(boulevard_la_sultana_s2, 27, 27).
+calles(boulevard_la_sultana_s3, 28, 28).
+calles(boulevard_la_sultana_s4, 29, 29).
+calles(boulevard_la_sultana_s5, 30, 30).
 
 %% Avenida Las Palmeras, 3 secciones
-calles(avenida_las_palmeras_s1).
-calles(avenida_las_palmeras_s2).
-calles(avenida_las_palmeras_s3).
+calles(avenida_las_palmeras_s1, 31, 31).
+calles(avenida_las_palmeras_s2, 31, 31).
+calles(avenida_las_palmeras_s3, 32, 32).
 
 %% 1ra Avenida Norte, 1 seccion
-calles(primera_avenida_norte_s1).
+calles(primera_avenida_norte_s1, 33, 33).
 
 %% Avenida Antiguo Cuscatlan, 3 secciones
-calles(avenida_antiguo_cuscatlan_s1).
-calles(avenida_antiguo_cuscatlan_s2).
-calles(avenida_antiguo_cuscatlan_s3).
+calles(avenida_antiguo_cuscatlan_s1, 34, 34).
+calles(avenida_antiguo_cuscatlan_s2, 35, 35).
+calles(avenida_antiguo_cuscatlan_s3, 36, 36).
 
 %% 3 Avenida Norte, 3 secciones
-calles(tercera_avenida_norte_s1).
-calles(tercera_avenida_norte_s2).
-calles(tercera_avenida_norte_s3).
+calles(tercera_avenida_norte_s1, 37, 37).
+calles(tercera_avenida_norte_s2, 38, 38).
+calles(tercera_avenida_norte_s3, 39, 39).
 
 
 %% 1 Calle Oriente, 5 secciones
-calles(primera_calle_oriente_s1).
-calles(primera_calle_oriente_s2).
-calles(primera_calle_oriente_s3).
-calles(primera_calle_oriente_s4).
-calles(primera_calle_oriente_s5).
+calles(primera_calle_oriente_s1, 40, 40).
+calles(primera_calle_oriente_s2, 41, 41).
+calles(primera_calle_oriente_s3, 42, 42).
+calles(primera_calle_oriente_s4, 43, 43).
+calles(primera_calle_oriente_s5, 44, 44).
 
 %% 1 Calle Poniente, 1 seccion
-calles(primera_calle_poniente_s1).
+calles(primera_calle_poniente_s1, 45, 45).
 
 %% Boulevard Walter Thilo Deininger, 7 secciones
-calles(boulevard_walter_thilo_deininger_s1).
-calles(boulevard_walter_thilo_deininger_s2).
-calles(boulevard_walter_thilo_deininger_s3).
-calles(boulevard_walter_thilo_deininger_s4).
-calles(boulevard_walter_thilo_deininger_s5).
-calles(boulevard_walter_thilo_deininger_s6).
-calles(boulevard_walter_thilo_deininger_s7).
+calles(boulevard_walter_thilo_deininger_s1, 46, 46).
+calles(boulevard_walter_thilo_deininger_s2, 47, 47).
+calles(boulevard_walter_thilo_deininger_s3, 48, 48).
+calles(boulevard_walter_thilo_deininger_s4, 49, 49).
+calles(boulevard_walter_thilo_deininger_s5, 50, 50).
+calles(boulevard_walter_thilo_deininger_s6, 50, 50).
+calles(boulevard_walter_thilo_deininger_s7, 51, 51).
 
 %% Calle Cuscatlan Poniente, 1 seccion
-calles(calle_cuscatlan_poniente_s1).
+calles(calle_cuscatlan_poniente_s1, 52, 52).
 
 %% Carretera Panamericana, 14 secciones
-calles(carretera_panamericana_s1).
-calles(carretera_panamericana_s2).
-calles(carretera_panamericana_s3).
-calles(carretera_panamericana_s4).
-calles(carretera_panamericana_s5).
-calles(carretera_panamericana_s6).
-calles(carretera_panamericana_s7).
-calles(carretera_panamericana_s8).
-calles(carretera_panamericana_s9).
-calles(carretera_panamericana_s10).
-calles(carretera_panamericana_s11).
-calles(carretera_panamericana_s12).
-calles(carretera_panamericana_s13).
-calles(carretera_panamericana_s14).
+calles(carretera_panamericana_s1, 53, 53).
+calles(carretera_panamericana_s2, 54, 54).
+calles(carretera_panamericana_s3, 55, 55).
+calles(carretera_panamericana_s4, 56, 56).
+calles(carretera_panamericana_s5, 57, 57).
+calles(carretera_panamericana_s6, 58, 58).
+calles(carretera_panamericana_s7, 59, 59).
+calles(carretera_panamericana_s8, 60, 60).
+calles(carretera_panamericana_s9, 61, 61).
+calles(carretera_panamericana_s10, 62, 62).
+calles(carretera_panamericana_s11, 63, 63).
+calles(carretera_panamericana_s12, 64, 64).
+calles(carretera_panamericana_s13, 65, 65).
+calles(carretera_panamericana_s14, 66, 66).
 
 %% Boulevard los Proceres, 5 secciones
-calles(boulevard_los_proceres_s1).
-calles(boulevard_los_proceres_s2).
-calles(boulevard_los_proceres_s3).
-calles(boulevard_los_proceres_s4).
-calles(boulevard_los_proceres_s5).
+calles(boulevard_los_proceres_s1, 67, 67).
+calles(boulevard_los_proceres_s2, 68, 68).
+calles(boulevard_los_proceres_s3, 69, 69).
+calles(boulevard_los_proceres_s4, 70, 70).
+calles(boulevard_los_proceres_s5, 71, 71).
 
 %% Boulevard Monsenor Romero, 4 secciones
-calles(boulevard_monsenor_romero_s1).
-calles(boulevard_monsenor_romero_s2).
-calles(boulevard_monsenor_romero_s3).
-calles(boulevard_monsenor_romero_s4).
+calles(boulevard_monsenor_romero_s1, 72, 72).
+calles(boulevard_monsenor_romero_s2, 73, 73).
+calles(boulevard_monsenor_romero_s3, 74, 74).
+calles(boulevard_monsenor_romero_s4, 75, 75).
 
 
 %% Boulevard Altamira, 1 seccion
-calles(boulevard_altamira_s1).
+calles(boulevard_altamira_s1, 76, 76).
 
 %% Calle Los Viveros, 1 seccion
-calles(calle_los_viveros_s1).
+calles(calle_los_viveros_s1, 77, 77).
 
 %% Calle Los Estadi, 1 seccion
-calles(calle_los_estadi_s1).
+calles(calle_los_estadi_s1, 78, 78).
 
 %% Calle Antigua Ferrocaril, 2 secciones
-calles(calle_antigua_ferrocaril_s1).
-calles(calle_antigua_ferrocaril_s2).
+calles(calle_antigua_ferrocaril_s1, 79, 79).
+calles(calle_antigua_ferrocaril_s2, 80, 80).
 
 %% Calle San Jeronimo Emiliani, 1 seccion
-calles(calle_san_jeronimo_emiliani_s1).
+calles(calle_san_jeronimo_emiliani_s1, 81, 81).
 
 %% Calle las Azucenas, 1 seccion
-calles(calle_las_azucenas_s1).
+calles(calle_las_azucenas_s1, 82, 82).
 
 %% Avenida Rio Amazonas, 1 seccion
-calles(avenida_rio_amazonas_s1).
+calles(avenida_rio_amazonas_s1, 83, 83).
 
 %% TODO: asigar las coordenadas
 %% Coordenadas de los lugares, x, y
@@ -593,3 +593,15 @@ ir_hacia2(X, Y, Acc, Route):-
     assert(eslabon(Z)),
     ir_hacia2(Z, Y, [Z|Acc], Route),
     !.
+
+calle_coordenada_x(Calle,  X, _) :-
+    calles(Calle, X, _).
+
+calle_coordenada_y(Calle, _, Y) :-
+    calles(Calle, _, Y).
+
+calle_coordenadas(Calle, List) :-
+    calle_coordenada_x(Calle, X, _),
+    calle_coordenada_y(Calle, _, Y),
+    List = [X, Y].
+
