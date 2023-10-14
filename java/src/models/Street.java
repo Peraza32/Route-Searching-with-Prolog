c012488507414d589972dbbb3bf63de88f8faa96
rivasdiego-dev <rivasdiego.dev@gmail.com>
2023-10-13 23:57:39 -0600

    :construction: Work on street class

--- java/src/models/Street.java
+++ java/src/models/Street.java
@@ -1,42 +1,63 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 
+// Class defining the streets
 public class Street {
+	// Name of the street
 	private String name;
-	private String segment;
-	private List<Coordinates> breakPoints;
+	// Breakpoints on the street
+	private List<Coordinates> breakPoints = new ArrayList<Coordinates>();
 	
-	public Street(String name, String segment, List<Coordinates> breakPoints) {
+	// Constructor
+	public Street(String name, List<Coordinates> breakPoints) {
 		super();
 		this.name = name;
-		this.segment = segment;
 		this.breakPoints = breakPoints;
 	}
 
+	// Setters and Getters
+	/**
+	 * @return the name of the street
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name the name of the street to set
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getSegment() {
-		return segment;
-	}
-
-	public void setSegment(String segment) {
-		this.segment = segment;
-	}
-
+	/**
+	 * @return Street breakPoints as list
+	 */
 	public List<Coordinates> getBreakPoints() {
 		return breakPoints;
 	}
 
+	/**
+	 * @param breakPoints the breakPoints to set
+	 */
 	public void setBreakPoints(List<Coordinates> breakPoints) {
 		this.breakPoints = breakPoints;
 	}
 	
+	
+	public List<Coordinates> getSegment(int segment) {
+	    List<Coordinates> segmentCoordinates = new ArrayList<Coordinates>();
+
+	    if (segment >= 0 && segment < this.breakPoints.size() - 1) {
+	        segmentCoordinates.add(this.breakPoints.get(segment));
+	        segmentCoordinates.add(this.breakPoints.get(segment + 1));
+	    } else {
+	        return null;
+	    }
+
+	    return segmentCoordinates;
+	}
 	
 }
